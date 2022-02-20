9b303ade5dbd5d8b0291da85f815b3370b894edb
Nick Brendholdt Rasmussen <nick-ras@mail.dk>
2022-02-20 15:01:30 +0100

    implemented while loop to continue or stop game

--- Program.cs
+++ Program.cs
@@ -9,20 +9,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            List<string> listOfSlotSymbols = new List<string>() {"cherrie", "grape", "orange", "melon", "lemons", "Aces", "King", "Queen", "Jack"};
 
-            var randWord = new Random(listOfSlotSymbols.Count);
-            string[,] pullHandle2D = new string[3, 3];
+            int moneyAvailable;
 
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine("Welcome to the game\nHow much do you want to play for? Enter an integer");
+            moneyAvailable = Convert.ToInt32(Console.ReadLine());
+
+            bool continueToPlay = true;
+
+            while (continueToPlay = true)
             {
-                for (int j = 0; j < 3; j++)
+                Console.WriteLine("Pull the handle by pressing enter");
+
+                List<string> listOfSlotSymbols = new List<string>() { "cherrie", "grape", "orange", "melon", "lemons", "Aces", "King", "Queen", "Jack" };
+
+                var randWord = new Random(listOfSlotSymbols.Count);
+                string[,] pullHandle2D = new string[3, 3];
+
+                for (int i = 0; i < 3; i++)
                 {
-                    pullHandle2D[i, j] = listOfSlotSymbols[randWord.Next(0, 9)];
-                    Console.Write(pullHandle2D[i, j] + "  ");
+                    for (int j = 0; j < 3; j++)
+                    {
+                        pullHandle2D[i, j] = listOfSlotSymbols[randWord.Next(0, 9)];
+                        Console.Write(pullHandle2D[i, j] + "  ");
+                    }
+                    Console.WriteLine("");
                 }
-                Console.WriteLine("");
             }
+            
            
         }
     }
