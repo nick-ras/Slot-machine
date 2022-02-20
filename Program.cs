c263b835ff4614da4db1aa378453edd393af94a1
Nick Brendholdt Rasmussen <nick-ras@mail.dk>
2022-02-20 14:51:09 +0100

    cleaning up

--- Program.cs
+++ Program.cs
@@ -14,36 +14,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
             var randWord = new Random(listOfSlotSymbols.Count);
             string[,] pullHandle2D = new string[3, 3];
 
-            pullHandle2D[2,2] = "cherry";
-            Console.WriteLine(pullHandle2D[2,2]);
-            Console.WriteLine(listOfSlotSymbols.Count + "\n\n");
-            // i checked that pullHandle had different random generated values than nineRandomSlotSymbols 
-
-
-
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     pullHandle2D[i, j] = listOfSlotSymbols[randWord.Next(0, 9)];
-                    Console.WriteLine(pullHandle2D[i, j]);
+                    Console.Write(pullHandle2D[i, j] + "  ");
                 }
+                Console.WriteLine("");
             }
-            
-            /*
-            for (int i = 0; i < pullHandle.Length; i++)
-            {
-                Console.Write($"{pullHandle[i]}  ");
-                if (i > 0 && (i+1)%3==0)
-                {
-                    Console.WriteLine("");
-                }
-
-            }*/
-
            
-
-
         }
     }
 }
