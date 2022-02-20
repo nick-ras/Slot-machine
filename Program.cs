7d4ef6ebab66e3d3b6648dff1dea0d70dcc2e0ea
Nick Brendholdt Rasmussen <nick-ras@mail.dk>
2022-02-20 16:19:14 +0100

    trying to check if lines have only one distinct value

--- Program.cs
+++ Program.cs
@@ -20,26 +20,38 @@ namespace MyApp // Note: actual namespace depends on the project name.
             while (continueToPlay = true)
             {
                 Console.WriteLine("Press any key to pull handle");
-
+                
                 string pullHandle = Convert.ToString(Console.ReadLine());
                
 
                 List<string> listOfSlotSymbols = new List<string>() { "cherrie", "grape", "orange", "melon", "lemons", "Aces", "King", "Queen", "Jack" };
 
                 var randWord = new Random(listOfSlotSymbols.Count);
-                string[,] pullHandle2D = new string[3, 3];
-
-                for (int i = 0; i < 3; i++)
+                string[,] outcomePullHandle2D = new string[3, 3];
+                
+                for (int i = 0; i < outcomePullHandle2D.GetLength(0); i++)
                 {
-                    for (int j = 0; j < 3; j++)
+                    for (int j = 0; j < outcomePullHandle2D.GetLength(1); j++)
                     {
-                        pullHandle2D[i, j] = listOfSlotSymbols[randWord.Next(0, 9)];
-                        Console.Write(pullHandle2D[i, j] + "  ");
+                        outcomePullHandle2D[i, j] = listOfSlotSymbols[randWord.Next(0, 9)];
+                        Console.Write(outcomePullHandle2D[i, j] + "  ");
                     }
                     Console.WriteLine("");
                 }
 
-                Console.WriteLine("This is the output\nDo you wish to play again");
+                foreach (string symbol in listOfSlotSymbols)
+                {
+                    for (int i = 0; i < outcomePullHandle2D.GetLength(0); i++)
+                    {
+                        if (outcomePullHandle2D[0,i].Distinct().Count() == 1)
+                        {
+                            Console.WriteLine("You got a straight row");
+                        }
+                    }
+                }
+
+
+                Console.WriteLine($"Congrats, you have {moneyAvailable} $. Do you wish to play again");
                 string answer = Convert.ToString(Console.ReadLine());
                 if (answer == "y")
                 {
@@ -50,8 +62,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     break;
                 }
             }
-            
-           
+
+
+
         }
     }
 }
